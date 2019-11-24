34e10f4e154dbb1f190db5340efc5c4b0cbba078
Rohit Nagvenkar <rohitsnagvenkar@gmail.com>
2019-11-24 15:21:30 -0600

    added navbar n sidebar

--- HealthMonitoring/src/app/app.component.ts
+++ HealthMonitoring/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HealthMonitoring';
+  title = 'Health Monitoring';
+  subtitle = 'Business Intelligence Dashboard';
 }
