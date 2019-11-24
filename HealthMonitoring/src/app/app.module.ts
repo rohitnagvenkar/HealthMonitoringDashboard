7727867306d45c156c37a37225317325013d53cd
Rohit Nagvenkar <rohitsnagvenkar@gmail.com>
2019-11-24 17:22:44 -0600

    added routes component

--- HealthMonitoring/src/app/app.module.ts
+++ HealthMonitoring/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from '../routes';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SectionHealthComponent } from '../app/sections/section-health/section-health.component';
+import { SectionOrdersComponent } from '../app/sections/section-orders/section-orders.component';
+import { SectionSalesComponent } from '../app/sections/section-sales/section-sales.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    SectionHealthComponent,
+    SectionOrdersComponent,
+    SectionSalesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
