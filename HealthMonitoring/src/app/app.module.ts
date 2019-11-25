41c5bdd161b881f3948e97d6256fe29228a46860
Rohit Nagvenkar <rohitsnagvenkar@gmail.com>
2019-11-24 21:23:14 -0600

    added chart components

--- HealthMonitoring/src/app/app.module.ts
+++ HealthMonitoring/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from '../routes';
+import { ChartsModule} from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -9,6 +10,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { SectionHealthComponent } from '../app/sections/section-health/section-health.component';
 import { SectionOrdersComponent } from '../app/sections/section-orders/section-orders.component';
 import { SectionSalesComponent } from '../app/sections/section-sales/section-sales.component';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +21,15 @@ import { SectionSalesComponent } from '../app/sections/section-sales/section-sal
     SidebarComponent,
     SectionHealthComponent,
     SectionOrdersComponent,
-    SectionSalesComponent
+    SectionSalesComponent,
+    BarChartComponent,
+    LineChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
