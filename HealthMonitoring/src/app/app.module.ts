0f4065bd7d0aa284d641c7365430e2973bec7a7f
Rohit Nagvenkar <rohitsnagvenkar@gmail.com>
2019-11-25 00:09:42 -0600

    created server objects

--- HealthMonitoring/src/app/app.module.ts
+++ HealthMonitoring/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { SectionSalesComponent } from '../app/sections/section-sales/section-sal
 import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
 import { LineChartComponent } from './charts/line-chart/line-chart.component';
 import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
     SectionSalesComponent,
     BarChartComponent,
     LineChartComponent,
-    PieChartComponent
+    PieChartComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
